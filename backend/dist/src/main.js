94987ebc60f84a0f70e972b42fa6f372a3e19629
hanfauzi <nifauzi45@gmail.com>
2026-07-13 14:58:24 +0700

    fix(jwt): remove all hard coded jwt

--- backend/dist/src/main.js
+++ backend/dist/src/main.js
@@ -1,12 +1,31 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
+const helmet_1 = __importDefault(require("helmet"));
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.use((0, helmet_1.default)({
+        contentSecurityPolicy: process.env.NODE_ENV === 'production' ? undefined : false,
+    }));
+    const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173'];
+    const frontendUrlEnv = process.env.FRONTEND_URL;
+    if (frontendUrlEnv) {
+        allowedOrigins.push(...frontendUrlEnv.split(',').map((url) => url.trim()));
+    }
     app.enableCors({
-        origin: '*',
+        origin: (origin, callback) => {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            }
+            else {
+                callback(new Error('Blocked by CORS policy'));
+            }
+        },
         credentials: true,
     });
     app.useGlobalPipes(new common_1.ValidationPipe({ transform: true }));
